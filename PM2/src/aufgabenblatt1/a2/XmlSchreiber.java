8a98c96568ff13e370470c94293b3ec0a48a198d
Adem Dagdeviren <adem.da40@gmail.com>
2016-09-30 16:26:52 +0200

    XML Schreiber 1.1

--- PM2/src/aufgabenblatt1/a2/XmlSchreiber.java
+++ PM2/src/aufgabenblatt1/a2/XmlSchreiber.java
@@ -1,6 +1,8 @@
 package aufgabenblatt1.a2;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,25 +15,36 @@ public class XmlSchreiber {
 	
 
 	
-	public XmlSchreiber(String filepath) throws ParserConfigurationException {
+	public XmlSchreiber(String filepath) {
 		
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		docFactory.setValidating(true);
 
 
-		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		DocumentBuilder docBuilder = null;
+		try {
+			docBuilder = docFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+		
 		Document document = docBuilder.newDocument();
 		Element rootElement = document.createElement("Sensor");
 		document.appendChild(rootElement);
-		rootElement.setNodeValue("Messung");
-		rootElement.setNodeValue("Messung");
+		rootElement.setAttribute("id", "Banane");
 		
+	}
+	
+	public Messungen messungen(){
+		List<Messungen> messung = new ArrayList<Messungen>();
+		messung.add(new Messungen(20,LocalDateTime.now()));
+		messung.add(new Messungen(22,LocalDateTime.now()));
+		messung.add(new Messungen(24,LocalDateTime.now()));
+		return messung;
 		
-		
-		
-		
-		
+	
 	}
 	
 	
